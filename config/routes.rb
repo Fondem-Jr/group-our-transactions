54ee13531bd2152de919d638768aa814225704fc
Fondem Junior <fondemjunior@yahoo.com>
2021-07-27 13:50:49 +0100

    Create a login and registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  #get 'admin/index'
+  #get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  root 'admin#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
